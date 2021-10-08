1e2786dbb7ce0fa75f3ea669ef6400b0eb7f85d3
William BRISA <funfake.electronics@gmail.com>
2021-10-08 08:40:07 +0200

    initial

---
+++ cmake-build-debug/CMakeFiles/TP3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP3.dir/main.cpp.obj"
+  "TP3.exe"
+  "TP3.exe.manifest"
+  "TP3.pdb"
+  "libTP3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
